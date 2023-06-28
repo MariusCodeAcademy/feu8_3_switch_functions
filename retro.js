2c2f9a62cff09e9b6a0f33f79bea775ad9b9e253
Marius CA <mariuskcodeacademy@gmail.com>
2023-06-28 11:39:47 +0300

    asd

---
+++ retro.js
@@ -0,0 +1,16 @@
+'use strict';
+console.log('retro.js file was loaded');
+
+// 1. sukurti funkcija atim() kuri gauna 2 argumentus
+// ir atima pira is antro, atspausdina rezultata
+
+// 2. skurti funkcija kuri sudaugina 3 argumentais
+// gautus skaicius
+
+// 3. sukurti funkcija kuri gauna 2 argumentus,
+// grazina ju vidurki, patikrinti ar gryzta rez.
+
+// 4. padaryti kad 1 ir 2 funkcijos grazintu
+// rezultatus. panaudoti 1 ir 2 funkcijas ir issaugoti
+// ju rezultatus ir juos panaudoti kaip reiksmes apskaiciuoti
+// 3 funkcija. rezultata prilyginti kintamajam 'galutinis', atspausdinti.
